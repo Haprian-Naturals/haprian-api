605d110d3bb459c4cb473d4ea7e47b4ad96981b5
mrhihihi <gabriel.kyeremateng.gk@gmail.com>
2025-05-05 19:32:29 +0000

    configured the cors middleware

--- index.js
+++ index.js
@@ -4,20 +4,6 @@ import cors from "cors";
 import "dotenv/config";
 import router from "./routes/index.js";
 
-//Database connection
-// async function connectionDatabase() {
-//   try {
-//       await mongoose.connect(process.env.MONGO_URI, {
-//           useNewUrlParser: true,
-//           useUnifiedTopology: true,
-//       });
-//       console.log("Database is connected");
-//   } catch (error) {
-//       console.error("Database connection error:", error.message); // Log the error message
-//       throw new Error("Database is NOT connected!!"); // Adjusted to only throw a new error with the message
-//   }
-// }
-
 await mongoose.connect(process.env.MONGO_URI)
 .then(data => console.log('Database connected'))
 .catch(err => console.log('error'))
@@ -25,19 +11,7 @@ await mongoose.connect(process.env.MONGO_URI)
 // CREATE AN EXPRESS APP
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  
-];
-
-app.use(
-  cors({
-    origin: allowedOrigins,
-    
-  })
-);
-
-app.options("*", cors());
+app.use(cors());
 
 // USE GLOBAL MIDDLEWARES
 app.use(express.json());
